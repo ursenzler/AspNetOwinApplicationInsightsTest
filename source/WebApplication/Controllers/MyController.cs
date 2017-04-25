805ac192bcbb5bca049382208533c87aae551558
Urs Enzler <urs.enzler@gmail.com>
2017-04-25 16:31:04 +0200

    exception test
    exception telemetry not correlated correctly with request. In request view on app insights there is no exception data available. Navigation over "show all telemetry for this operation" is possible.

---
+++ source/WebApplication/Controllers/MyController.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    [RoutePrefix("test")]
+    public class MyController : ApiController
+    {
+        public MyController()
+        {
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IEnumerable<string> Get()
+        {
+            throw new Exception("intentional exception");
+        }
+    }
+}
---
+++ source/WebApplication/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication
+{
+    using System.Web.Http;
+    using System.Web.Http.Dependencies;
+    using System.Web.Http.Filters;
+    using ApplicationInsights.OwinExtensions;
+    using Microsoft.ApplicationInsights;
+    using Owin;
+    using WebApplication.Controllers;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseApplicationInsights();
+
+            var configuration = new HttpConfiguration
+            {
+                DependencyResolver = new DependencyResolver()
+            };
+            
+            configuration.MapHttpAttributeRoutes();
+            configuration.Filters.Add(new ExceptionLoggingFilter());
+
+            app.UseWebApi(configuration);
+        }
+    }
+
+    public class DependencyResolver : IDependencyResolver
+    {
+        public void Dispose()
+        {
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(MyController))
+            {
+                return new MyController();
+            }
+
+            return null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            yield break;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+    }
+
+    public class ExceptionLoggingFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            var telemetry = new TelemetryClient();
+
+            telemetry.TrackException(actionExecutedContext.Exception);
+
+            base.OnException(actionExecutedContext);
+        }
+    }
+}
